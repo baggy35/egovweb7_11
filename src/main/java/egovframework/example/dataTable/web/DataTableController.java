c0677cb00905aca093994aa7e305ecc7dc61199d
baggy35 <baggy33@naver.com>
2018-07-11 10:39:04 +0900

    ajax설정

--- src/main/java/egovframework/example/dataTable/web/DataTableController.java
+++ src/main/java/egovframework/example/dataTable/web/DataTableController.java
@@ -2,6 +2,10 @@ package egovframework.example.dataTable.web;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import javax.servlet.ServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +13,47 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DataTableController {
+	@RequestMapping("/dataTable.do")
+	 public String dataTable() throws Exception {
+		 
+		  
+		  return "dataTable/list";
+	 }
+	
 	@RequestMapping("/ajax.do")
-	 public @ResponseBody Map<String, Object> ajax() throws Exception {
+	 public @ResponseBody Map<String, Object> ajax(ServletRequest req) throws Exception {
+		dispParams(req);
 		  Map map = new HashMap<String, Object>();
+		  
 		  map.put("AA", "1");
 		  map.put("BB", "2");
 		  map.put("CC", "3");
 		  map.put("DD", "4");
 		  return map;
 	 }
+	
+	private void dispParams(ServletRequest req)
+	{
+		Map<String, String[]> map = req.getParameterMap();
+		SortedSet<String> set = new TreeSet<String>();
+		set.addAll(map.keySet());
+		
+		for(String key : set)
+		{
+			String[] values = map.get(key);
+			System.out.print(key);
+			System.out.print(" : ");
+			for(String value : values)
+			{
+				if (value != null)
+				{
+					System.out.print(value);
+					System.out.print(",");
+				}
+			}
+			System.out.println();
+		}
+	}
 }
+
+
